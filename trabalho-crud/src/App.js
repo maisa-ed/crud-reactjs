8893a70ad258de977a47f40a355f600cb397ced0
maisa-ed <maisaeduarda080@gmail.com>
2023-12-12 16:55:28 -0300

    teste

--- trabalho-crud/src/App.js
+++ trabalho-crud/src/App.js
@@ -1,119 +1,24 @@
-import logo from "./logo.svg";
-import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
-import "./App.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import './App.css';
+import Create from './components/create';
+import Read from './components/read';
+import Update from './components/update';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
-  const Todos = ({ todos }) => {
-    return (
-      <div className="todos">
-        {todos.map((todo) => {
-          return (
-            <div className="todo">
-              <button
-                onClick={() => modifyStatusTodo(todo)}
-                className="checkbox"
-                style={{ backgroundColor: todo.status ? "#A879E6" : "white" }}
-              ></button>
-              <p>{todo.name}</p>
-              <button onClick={() => handleWithEditButtonClick(todo)}>
-                <AiOutlineEdit size={20} color={"#64697b"}></AiOutlineEdit>
-              </button>
-              <button onClick={() => deleteTodo(todo)}>
-                <AiOutlineDelete size={20} color={"#64697b"}></AiOutlineDelete>
-              </button>
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-
-  async function handleWithNewButton() {
-    console.log("fasfas");
-    setInputVisility(!inputVisbility);
-  }
-  async function handleWithEditButtonClick(todo) {
-    setSelectedTodo(todo);
-    setInputVisility(true);
-  }
-  async function getTodos() {
-    const response = await axios.get("http://localhost:3333/todos");
-    setTodos(response.data);
-    console.log(response.data);
-  }
-  async function editTodo() {
-    const response = await axios.put("http://localhost:3333/todos", {
-      id: selectedTodo.id,
-      name: inputValue,
-    });
-    setSelectedTodo();
-    setInputVisility(false);
-    getTodos();
-    setInputValue("");
-  }
-  async function deleteTodo(todo) {
-    const response = await axios.delete(
-      `http://localhost:3333/todos/${todo.id}`
-    );
-    getTodos();
-  }
-  async function modifyStatusTodo(todo) {
-    const response = await axios.put("http://localhost:3333/todos", {
-      id: todo.id,
-      status: !todo.status,
-    });
-    getTodos();
-  }
-
-  async function createTodo() {
-    const response = await axios.post("http://localhost:3333/todos", {
-      name: inputValue,
-    });
-    getTodos();
-    setInputVisility(!inputVisbility);
-    setInputValue("");
-  }
-
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [inputVisbility, setInputVisility] = useState(false);
-  const [selectedTodo, setSelectedTodo] = useState();
-
-  useEffect(() => {
-    getTodos();
-  }, []);
-
   return (
-    <div className="App">
-      <header className="container">
-        <div className="header">
-          <h1>Dont be lazzy</h1>
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+        <div>
+          <Route exact path='/create' component={Create} />
         </div>
-        <Todos todos={todos}></Todos>
-        <input
-          value={inputValue}
-          style={{ display: inputVisbility ? "block" : "none" }}
-          onChange={(event) => {
-            setInputValue(event.target.value);
-          }}
-          className="inputName"
-        ></input>
-        <button
-          onClick={
-            inputVisbility
-              ? selectedTodo
-                ? editTodo
-                : createTodo
-              : handleWithNewButton
-          }
-          className="newTaskButton"
-        >
-          {inputVisbility ? "Confirm" : "+ New task"}
-        </button>
-      </header>
-    </div>
+        <div style={{ marginTop: 20 }}>
+          <Route exact path='/read' component={Read} />
+        </div>
+
+        <Route path='/update' component={Update} />
+      </div>
+    </Router>
   );
 }
 
